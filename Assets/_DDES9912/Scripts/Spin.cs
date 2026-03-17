0beeb7e2235100aca7189e9332f8edf28b4ad904
EasonC-HUB <easonchi008@gmail.com>
2026-03-17 11:43:16 +1100

    Week 2

--- Assets/_DDES9912/Scripts/Spin.cs
+++ Assets/_DDES9912/Scripts/Spin.cs
@@ -13,6 +13,6 @@ public class Spin : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0, Yspeed, 0);
+        transform.Rotate(0, Yspeed * Time.deltaTime, 0);
     }
 }
